a664aeab6184177c385aec6ebffc48c41cd42cc3
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-10 16:16:37 +0200

    update

--- Test Ordner/Typ_Casten.py
+++ Test Ordner/Typ_Casten.py
@@ -1,4 +1,11 @@
-wert1 = float(input("Erste Zahl eingeben: "))
+#float = Gleitkommazhl mit + Operator
+wert1 = float(input("Erste Zahl eingeben: ")) # durch das setzem vom float vor dem input wird die zeichen kette vom string zu einem float umgewandelt!
 wert2 = float(input("Zweite Zahl eingeben: "))
 
 print(wert1 + wert2)
+
+#alternativ so
+#wert1 =(input("Erste Zahl eingeben: "))
+#wert2 =(input("Zweite Zahl eingeben: "))
+
+#print(float(wert1) + float(wert2))
