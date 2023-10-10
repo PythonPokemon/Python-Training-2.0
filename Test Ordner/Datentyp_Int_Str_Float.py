ab3a6acf6de9c7dc6cfdf9d5c640d3e5ddd21e74
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-10 12:05:55 +0200

    update

---
+++ Test Ordner/Datentyp_Int_Str_Float.py
@@ -0,0 +1,10 @@
+# int, abgekürtzt = Integer sind ganzzahlen, also ohne komma
+int_variable = 100  
+
+# float sind Gleitkomma zahlen, also alle zahlen mit Kommarechnungen
+float_variable = 3,75 
+
+# str, abgekürtzt => string, sind Zeichenketten, bsp: Aneinanderreihung von Zahlen und Texten
+string_variable1 = 'Das ist ein String, mit 1 Zeichen'
+string_variable2 = "Das ist ein String, mit 2 Zeichen"
+string_variable3 = '''Das ist ein String, mit 3 Zeichen'''
