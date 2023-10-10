41ee884457471c595fae75d06dcffb4b92e9efde
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-10 15:29:57 +0200

    update

--- Test Ordner/input.py
+++ Test Ordner/input.py
@@ -2,4 +2,6 @@
 print("Anfang des Programms")
 name = input('Hallo, wie heißt du? ')
 print(" Hallo :)" + name )
+alter = input("Gib dein alter ein:")
+print(" dein alter ist :)" + alter )
 print("Ende des Programms")
