d694c1e29f63176bd5881ac4d0a564a822ba1d84
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 08:49:56 +0200

    update

---
+++ Listen/test_liste.py
@@ -0,0 +1,21 @@
+# Wiedeirholung Liste
+
+meine_liste = []
+print(type(meine_liste))
+
+# Index         1      2    3     4
+meine_liste = [1, "zwei", True, 2,34]
+
+# Index
+print(meine_liste[1])
+
+print(meine_liste[2])
+
+print(meine_liste[3])
+
+print(meine_liste[4])
+
+tiere = ["hund", "katze", "maus"]
+
+for tiere in tiere:
+    print(tiere)
