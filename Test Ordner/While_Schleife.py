97a8f45d624b78cdc61089e357245896977ff6b9
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 13:37:00 +0200

    update

---
+++ Test Ordner/While_Schleife.py
@@ -0,0 +1,10 @@
+# unendlichkeutsschleife
+# der code wird endlos ausgeführt da 1 kleier 10 ist = also True, Richtig!
+
+zähler = 1 
+while zähler < 10: 
+    print("Der Code wird endlos widerholt")
+    zähler += 1 
+# diese funktion zählt immer +1, bedeutet also ab 1 ++++++++++ = 10, 
+# wenn die schleife das 10te mal die schleifen durchlaufen hat wird die schliefe beendet
+# bedeutet im umkehrschluss das ohne diesen zähkler die schleife endloss wäre
