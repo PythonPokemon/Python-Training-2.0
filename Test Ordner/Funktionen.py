2b6ca1892efd495220cc6b2aa1fe16a80f1c263d
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 15:48:42 +0200

    update

--- Test Ordner/Funktionen.py
+++ Test Ordner/Funktionen.py
@@ -3,6 +3,8 @@
 # die einzelen Funktionalitäten in unterschiedliche Funktionen Auslagern!
 
 def sag_hallo():
-    print('Willkommen zurück mein Liebling..\nWo warst du?\n')
+    print('Willkommen zurück mein Liebling..\nWo warst du?\n') # hier wird die def funktion deklariert
 
-sag_hallo()
+sag_hallo() # hier wird die def funktion abgerufen 
+
+print('Programm Ende')
