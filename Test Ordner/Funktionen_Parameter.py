464381ddaa3a20652416fb1a6856f6c436342cae
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 16:19:49 +0200

    update

--- Test Ordner/Funktionen_Parameter.py
+++ Test Ordner/Funktionen_Parameter.py
@@ -1,15 +1,17 @@
 # Was sind Funktionen?
-# Das sind wie unterprogramme, 
-# die einzelen Funktionalitäten in unterschiedliche Funktionen Auslagern!
+# Das sind wie Unterprogramme,
+# die einzelnen Funktionalitäten in unterschiedliche Funktionen auslagern!
 
 # Reihenfolge beachten:
-#                1         2          3
-def sag_hallo(vorname, nachname, taubenname):
-    print('Hallo' + vorname + nachname + ' Willkommen zurück')
-    print('Wo ist deine Brieftaube ' + taubenname + ' ? XD')
+#                1         2          3           4         5
+def sag_hallo(vorname, nachname, taubenname, geburtstag, präsident):
+    print('Hallo ' + vorname + nachname + ' Willkommen zurück')
+    print('Wo ist deine Brieftaube ' + taubenname + '? XD')
+    print('Hat der nicht am 7. ' + geburtstag + ' Geburtstag?')
+    print('Wie ' + präsident + '?')
 
 # Reihenfolge beachten
-#           1           2              3
-sag_hallo(' Ali', ' Vonderlayen', ' Hassan') # funktions aufruf + integrierte parameter, string
+#           1           2              3       4         5
+sag_hallo('Ali', ' Vonderlayen', 'Hassan', 'Oktober', 'Putin') # Funktionsaufruf + integrierte Parameter, String
 
 print('Programm Ende')
