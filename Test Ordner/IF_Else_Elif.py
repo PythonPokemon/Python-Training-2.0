c1fdc2567343e1192b749c7ef6008d7d318803d3
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 11:25:39 +0200

    update

--- Test Ordner/IF_Else_Elif.py
+++ Test Ordner/IF_Else_Elif.py
@@ -1,7 +1,18 @@
+
+aufgabe_1 = ("Aufgabe 1, Altersabfrage:")
+print(aufgabe_1)
+
 alter = int(input("Bitte gib deinen Namen ein: "))
 
-if alter < 18:
+if alter < 18:# alles kleinr 18
     print("Achtung der Nutzer ist unter 18 Jahre alt!")
-    alter = 99
+
+elif alter == 18: # 18 gleich 18
+    print("Der Nutzer ist exakt 18 Jahre alt")
+elif alter == 19: # 19 gleich 19
+    print("Der Nutzer ist exakt 19 Jahre alt")
+
+else: # alle ab 20 Jahre fallen hier rein
+    print("Der Nutzer ist volljährig")
 
 print("Programmende")
