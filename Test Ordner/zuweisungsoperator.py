f41a50637cf85cd7ff425b3ca150aa35ad28e3bf
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 09:21:23 +0200

    update

--- Test Ordner/zuweisungsoperator.py
+++ Test Ordner/zuweisungsoperator.py
@@ -2,12 +2,16 @@
 
 beispiel1 = 22
 beispiel2 = 22 + 3
-beispiel3 = (22 - 1) * 3
+beispiel3 = (22 - 2) * 3
 
 # variante 1 berechnung
 beispiel1 = beispiel1 +10
 print(beispiel1)
 
-# variante 2 berechnung
+# variante 2 berechnung, kurzschreibweise
 beispiel2 += 10
 print(beispiel2)
+
+# der obere wert [(22 - 2) * 3 = 60] geteilt durch 2 laut diesem wert = 30
+beispiel3 /= 2
+print(beispiel3)
