cca5296c0714c629534ded66c479ad2d37f5aee5
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 15:17:53 +0200

    update

--- Test Ordner/for_schleife.py
+++ Test Ordner/for_schleife.py
@@ -1,3 +1,12 @@
 
 # Variante 1: Iterierbare Objekte durchlaufen (zB. Listen)
 # Variante 2: Zählerschleife
+
+for zahlen in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1.2, 3.24]: #Integer, vertikale ausgabe
+    print(zahlen)
+
+for buchstaben in ["Ich bin ein string"]: # horitontale ausgabe
+    print(buchstaben)
+
+for buchstaben in "Ich bin ein string": # vertikale ausgabe
+    print(buchstaben)
