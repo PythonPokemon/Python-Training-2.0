a16dad82c007ac1992bb5800e63ebdbf8a380e34
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 14:04:54 +0200

    update

---
+++ Test Ordner/Zugriff_liste.py
@@ -0,0 +1,12 @@
+# Konzept
+# Variablen: Können 'einen' Wert speichern
+# Listen: Können 'mehrere' Werte speichern
+
+# Zahl      0       1          2       3      4       
+nammen = ['Ali', 'Hassan', 'Abduhl', 'Kai', 3.75] # integer und float
+print(nammen)
+
+print(nammen[0]) # = Ali |da explizites zugreifen auf die liste, erste nummer ist immer die 0 die Null
+print(nammen[1]) # Hassen
+print(nammen[-1]) # 3.75
+print(nammen[-2]) # Kai | da -2 bedeutet minus zwei von hinten abgezogen Bsp: 5-2 = 3
