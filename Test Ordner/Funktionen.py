50220be679a1e995e07f93c910b0653fbf78d494
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 15:43:03 +0200

    update

---
+++ Test Ordner/Funktionen.py
@@ -0,0 +1,8 @@
+# Was sind Funktionen?
+# Das sind wie unterprogramme, 
+# die einzelen Funktionalitäten in unterschiedliche Funktionen Auslagern!
+
+def sag_hallo():
+    print('Willkommen zurück mein Liebling..\nWo warst du?\n')
+
+sag_hallo()
