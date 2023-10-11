1e00cc93b6d490aa30d37d075a39ab869b816349
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 13:20:13 +0200

    update

--- Test Ordner/Logische_Operatoren.py
+++ Test Ordner/Logische_Operatoren.py
@@ -10,9 +10,9 @@ nummer_3 = int(input("Dritte Zahl, Bitte wähle eine zahl zwischen 1 und 30: "))
 
 #-Variante-1
 # jede IF-Else verzweigung muss auf der selben höhe verschachtelt werden!
-if nummer_1 == 3:
-    if nummer_2 == 7:
-        if nummer_3 == 21:
+if nummer_1 == 1:
+    if nummer_2 == 2:
+        if nummer_3 == 3:
             print("Juhhuuu, Du hast den Jackpot :-)")
         else:
             print("Du hast leider verloren, kannt für das Trinkgeld XD")
@@ -22,8 +22,8 @@ else:
     print("Du hast leider verloren, kannt für das Trinkgeld XD")
 
 #-Variante-2
-# smarte lösung und, oder, nicht Operation verwenden!
-if nummer_1 == 3 and nummer_2 == 7 and nummer_3 == 21:
+# smarte lösung und, oder, nicht Operation verwenden! (Gesamtergebnis muss True sein)
+if nummer_1 == 1 and nummer_2 == 2 and nummer_3 == 3:
     print("Herzlichen Glückwunsch, du hast die Lotterie gewonnen!")
 else:
     print("Du hast erneut rein gepayt XD")
