118b225d1c1c170d11958755cab8d2449965a93a
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 11:02:06 +0200

    update

--- Test Ordner/Vergleichsoperator_Bool.py
+++ Test Ordner/Vergleichsoperator_Bool.py
@@ -1,13 +1,32 @@
+aufgabe_1 = "Aufgabe 1, größer als"
+aufgabe_2 = "Aufgabe 2, kleiner als"
+aufgabe_3 = "Aufgabe 3, kleiner/größer oder gleich"
+aufgabe_4 = "Aufgabe 4, gleicher wert"
+aufgabe_5 = "Aufgabe 5, ungleicher wert"
+
 # Boole wert sagt ob etwas True / False ist also : wahr oder falsch
 
+print(aufgabe_1)
 # größer als >
 print(5 > 1) # größer als = True, denn 5 ist größer als 1!
 print(1 > 5) # größer als = False, denn 1 ist nicht größer als 5!
 
+print(aufgabe_2)
 # kleiner als <
 print(1 < 2) # kleiner als = True, denn 1 ist kleiner als 2!
 print(2 < 1) # kleiner als = False, denn 2 ist nicht kleiner als 1!
 
+print(aufgabe_3)
 # kleiner / größer oder gleich =
 print(1 >= 1) # 1 ist größer als 1 oder gleich = True, also wahr!
 print(1 <= 1) # 1 ist kleiner als 1 oder gleich = True, also wahr!
+
+print(aufgabe_4)
+# gleicher wert
+print ( 1 == 1) # True, da 1 der 1 entspricht.
+print ( 1 == 2) # False, da 1 der 2 nicht entspricht.
+
+print(aufgabe_5)
+# ungleicher wert
+print (1 != 2) # True, da 1 ungleich 2 ist.
+print (1 != 1) # False, da 1 nicht ungleich 1 ist.
