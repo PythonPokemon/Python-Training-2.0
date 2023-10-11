ee8eb862802e8c64e10051449075d70b761c47f1
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-11 13:50:54 +0200

    update

---
+++ Test Ordner/Listen.py
@@ -0,0 +1,11 @@
+# Konzept
+# Variablen: Können 'einen' Wert speichern
+# Listen: Können 'mehrere' Werte speichern
+
+nummern_liste = [3, 5, 10, 15] # strings
+print(nummern_liste)
+
+nammens_liste = ['Ali', 'Hassasn', 'Abduhl', 'Kai', 3.75] # integer und float
+print(nammens_liste)
+
+print(nummern_liste and nammens_liste)
