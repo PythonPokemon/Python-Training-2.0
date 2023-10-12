5384945b9790afffc3e9d8162bb6d9c491b8b958
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-12 09:42:53 +0200

    update

--- Test Ordner/Funktionen_mit_Rückgabewert.py
+++ Test Ordner/Funktionen_mit_Rückgabewert.py
@@ -1,18 +1,20 @@
+# Auskommentieren alles markieren Strg + A dann ( Strg + K + C)
+# Entkommentieren alles markieren Strg + A dann (Strg + K + U)
+
 # # Was sind Funktionen?
 # # Das sind wie Unterprogramme,
 # # die einzelnen Funktionalitäten in unterschiedliche Funktionen auslagern!
 
-# # Reihenfolge beachten:
-# #                1         2          3           4         5
-# def sag_hallo(vorname, nachname, taubenname, geburtstag, präsident):
-#     print('Hallo ' + vorname + nachname + ' Willkommen zurück')
-#     print('Wo ist deine Brieftaube ' + taubenname + '? XD')
-#     print('Hat der nicht am 7. ' + geburtstag + ' Geburtstag?')
-#     print('Wie ' + präsident + '?')
+# Reihenfolge oben und unten müssen eingehalten werden!
+#           1  2
+def maximum(a, b):
+    if a < b: # : müssen meistens zum abschluss stehen, es gibt ausnahmen!
+        return b
+    else:
+        return a
+#                  1  2
+ergebnis = maximum(4, 7) 
+print(ergebnis)
 
-# # Reihenfolge beachten
-# #           1           2              3       4         5
-# print(type(sag_hallo('Ali', ' Vonderlayen', 'Hassan', 'Oktober', 'Putin'))) # Funktionsaufruf + integrierte Parameter, String
-# #print davor gibt einen rückgabewert in der Konsole aus: 'None' = nichts!
-# # mit type davor, wird die classe 'NoneType' in der Konsole ausgegeben ! (BasisDatentyp)
-# print('Programm Ende')
+# wenn also a kleiner b ist entspricht 4 < 7
+# soll das maximum als ergebnis ausgegeben werden, aslo = 7
