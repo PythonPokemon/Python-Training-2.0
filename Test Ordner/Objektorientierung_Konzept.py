56651f108be7be8643d35510853005d3cbb68ce1
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-12 11:18:14 +0200

    update

--- Test Ordner/Objektorientierung_Konzept.py
+++ Test Ordner/Objektorientierung_Konzept.py
@@ -6,28 +6,28 @@
 # Klassen und Objekte
 
 # __init__ Methode == Funktion die Innerhalb der Klasse/Class bezeichnet wird
-class Auto: # Generele und erweiterbare Parameter für alle Autos
-    def __init__(self):
+class Auto: # 'Klasse' Generel und erweiterbar für alle Objekte!
+    def __init__(self): # definition der Attribute, für das Objekt!
         self.auto_marke = None # der Wert None steht für nichts,
         self.ps_stärke = None # damit die Parameter Spezifisch,
         self.auto_farbe = None # objektbezogen, bsp: auto1 oder auto2,
         self.auto_türen = None # eingegeben werden können!
 #-----------------------------------------------
-# Auto 1
-auto1 =Auto() # Eingabe Parameter
+# 'Objekt'-Auto 1
+auto1 = Auto() # Eingabe Attribute
 auto1.auto_marke = 'BMW M3'
 auto1.ps_stärke = 250
 auto1.auto_farbe = 'Perl Weiss'
 auto1.auto_türen = 3
 
-# Auto 1 = ausgabe der Parameter über print
+# Auto 1 = ausgabe der Attribute, über print
 print(auto1.auto_marke) 
 print(auto1.ps_stärke)
 print(auto1.auto_farbe)
 print(auto1.auto_türen)
 #-----------------------------------------------
-# Auto 2 Eingabe Parameter
-auto2 =Auto()
+# 'Objekt'Auto 2 Eingabe Parameter
+auto2 = Auto()
 auto2. auto_marke = 'Audi R8'
 auto2.ps_stärke = 300
 auto2.auto_farbe = 'Schwarz Carbon'
@@ -38,3 +38,6 @@ print(auto2.auto_marke)
 print(auto2.ps_stärke)
 print(auto2.auto_farbe)
 print(auto2.auto_türen)
+#-----------------------------------------------
+# usw...
+#auto3 = Auto()
