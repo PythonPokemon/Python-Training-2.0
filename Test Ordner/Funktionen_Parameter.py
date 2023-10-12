3a6de0d4fc8adac9961d3380064e4c23f1b27558
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-12 11:43:19 +0200

    update

--- Test Ordner/Funktionen_Parameter.py
+++ Test Ordner/Funktionen_Parameter.py
@@ -2,7 +2,7 @@
 # Das sind wie Unterprogramme,
 # die einzelnen Funktionalitäten in unterschiedliche Funktionen auslagern!
 
-# Reihenfolge beachten:
+# Reihenfolge beachten!!!
 #                1         2          3           4         5
 def sag_hallo(vorname, nachname, taubenname, geburtstag, präsident):
     print('Hallo ' + vorname + nachname + ' Willkommen zurück')
@@ -10,8 +10,8 @@ def sag_hallo(vorname, nachname, taubenname, geburtstag, präsident):
     print('Hat der nicht am 7. ' + geburtstag + ' Geburtstag?')
     print('Wie ' + präsident + '?')
 
-# Reihenfolge beachten
-#           1           2              3       4         5
+# Reihenfolge beachten!!!
+#                       1          2            3         4         5
 print(type(sag_hallo('Ali', ' Vonderlayen', 'Hassan', 'Oktober', 'Putin'))) # Funktionsaufruf + integrierte Parameter, String
 #print davor gibt einen rückgabewert in der Konsole aus: 'None' = nichts!
 # mit type davor, wird die classe 'NoneType' in der Konsole ausgegeben ! (BasisDatentyp)
